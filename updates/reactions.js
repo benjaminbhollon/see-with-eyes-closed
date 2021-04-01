37f5761a520be6e0869012ba7425deae7e011fcc
Benjamin Hollon <benjaminbhollon@gmail.com>
2021-04-01 23:35:54 +0800

    Update outdated articles with empty reactions

---
+++ updates/reactions.js
@@ -0,0 +1,27 @@
+const config = require('../config.json');
+const crud = require('@bibliobone/mongodb-crud').bind(config.mongodbURI, 'swec-core');
+
+crud.updateMultipleDocuments('articles', { reactions: { $exists: false } }, { reactions: [
+  {
+    name: "impressive",
+    count: 0
+  },
+  {
+    name: "funny",
+    count: 0
+  },
+  {
+    name: "hooray",
+    count: 0
+  },
+  {
+    name: "silly",
+    count: 0
+  },
+  {
+    name: "avocado",
+    count: 0
+  }
+] }).then(result => {
+  console.log(result);
+})
